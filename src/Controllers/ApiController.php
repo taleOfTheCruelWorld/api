d5aecd1c62ca8d627c3564c0ac57356be82ac6bf
Vlados <home@da.da>
2025-12-06 12:56:27 +0700

    Api3

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -8,31 +8,28 @@ use Psr\Http\Message\ResponseInterface;
 class ApiController extends Controller
 {
     public function getBuilding(RequestInterface $request, ResponseInterface $response, $args) {
+        $complex = \ORM::forTable('complex')->where('slug',$args['slug'])->findOne();
+        if (!$complex) {
+            $data = null;
+            $response->getBody()->write(json_encode($data));
+            return $response->withHeader('Content-Type', 'aplication/json');
+        }
+        $buildings = \ORM::forTable('buildings')->where('complex_id', $complex['id'])->findArray();
+        $layouts= \ORM::forTable('layouts')->where('complex_id',$complex['id'])->findArray();
         $data = [
-            'name' => 'Complex',
-            'adress'=> 'dadsa',
-            'latitude'=>'41,312',
-            'longtude'=>'321,4',
-            'sectors' => [
-                [
-                'id'=> '2',
-                'planning' => '2025-02-31',
-                'floors' => '15'
-                ]
-            ],
-            'layouts' => [
-                [
-                    'id'=>'123',
-                    'image'=>'https://da.dsa',
-                ]
-            ]
+            'name' => $complex['name'],
+            'adress'=> $complex['adress'],
+            'latitude'=>$complex['latitude'],
+            'longtude'=>$complex['longtude'],
+            'sectors' => $buildings,           
+            'layouts' => $layouts       
         ];
         $response->getBody()->write(json_encode($data));
         return $response->withHeader('Content-Type', 'aplication/json');
     }
     public function getApartments(RequestInterface $request, ResponseInterface $response, $args) {
-        $data = \ORM::forTable('apartments')->join('image_of_apartments', array('image_of_', '=', 'p2.id'))->findArray();
+        $data = \ORM::forTable('apartments')->rawQuery('SELECT ap.*, GROUP_CONCAT(im.image ORDER BY ap.id) AS images FROM `apartments` as ap LEFT JOIN images_of_apartments as im on ap.id=im.apartments_id GROUP BY ap.id')->findArray();
         $response->getBody()->write(json_encode($data));
-        return $response->withHeader('Content-Type', 'aplication/json');
+        return $response->withHeader('Content-Type', 'application/json');
     }
 }
